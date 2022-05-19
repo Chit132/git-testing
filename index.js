5e6ea81a7d543590f619f0e736fe152a2612b2b8
Chit <61895718+Chit132@users.noreply.github.com>
2022-05-19 14:02:47 -0400

    readme

--- index.js
+++ index.js
@@ -3,3 +3,4 @@ const dev = 'i like to make stuff';
 console.log('the original message is' + msg);
 console.log(dev + 'this was a message added in the dev branch');
 console.log('hi github');
+console.log('readme has been added')
